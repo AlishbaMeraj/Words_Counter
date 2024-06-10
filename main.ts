adfd08921ff9c92bb2c295214f6a402d3dc20c70
AlishbaMeraj <alishbameraj961@gmail.com>
2024-06-09 22:34:19 -0700

    This is Words Counter Application

---
+++ main.ts
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+// Display a colourfull welcome message
+console.log(chalk.bold.cyan("\n \t\t Welcome to 'Alishba Meraj' - word counter Application\n\t"));
+console.log("-*".repeat(40));
+
+// prompt the user to enter a sentence
+let answer = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter a Sentence",
+    }
+]);
+
+//  Trimming the sentence and spliting it into words based on "spaces"
+let words = answer.sentence.trim().split(" ");
+
+// analysis of user input sentence
+console.log("-".repeat(70));
+console.log(chalk.bold.italic("- sentence words:"));
+console.log(words);
+console.log(chalk.bold(`\n- word count: ${chalk.bold.magenta(words.length)}`));
+console.log("-".repeat(70));
+console.log(chalk.bold.cyan("\n\n\tThank you for using our Word counter Application . Have a great day!\n\n\t"));
